621f0bb0efa37c39a53ed608c7f4263b529cc171
Eshan Savla <eshan.savla@ipa.fraunhofer.de>
2025-03-15 23:15:57 +0100

    finished implementation

--- include/jigless-planner/TopControllerNode.hpp
+++ include/jigless-planner/TopControllerNode.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <utility>
 #include <fstream>
+#include <mutex>
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/node.hpp>
@@ -23,6 +24,9 @@
 #include <plansys2_planner/PlannerClient.hpp>
 #include <plansys2_problem_expert/ProblemExpertClient.hpp>
 
+#include "jigless_planner_interfaces/msg/joint_status.hpp"
+#include "jigless_planner_interfaces/srv/interact_top.hpp"
+
 namespace jigless_planner
 {
   class TopControllerNode : public rclcpp::Node
@@ -32,7 +36,10 @@ namespace jigless_planner
     ~TopControllerNode();
 
   private:
+    bool goal_changed_ = false, pause_ = false;
     std::vector<std::string> goal_joints;
+    std::map<std::string, bool> failed_joints;
+    std::mutex failed_joints_mutex_;
     typedef enum
     {
       STARTING,
@@ -47,23 +54,33 @@ namespace jigless_planner
     std::shared_ptr<plansys2::PlannerClient> planner_client_;
     std::shared_ptr<plansys2::ProblemExpertClient> problem_expert_;
     std::shared_ptr<plansys2::ExecutorClient> executor_client_;
+    
     // Add Server for goal adding/appending/removal etc
 
     std::shared_ptr<rclcpp::CallbackGroup> lifecycle_group;
     std::shared_ptr<rclcpp::CallbackGroup> action_group;
+    std::shared_ptr<rclcpp::CallbackGroup> interactor_group;
+    std::shared_ptr<rclcpp::CallbackGroup> executor_group;
 
     rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedPtr change_state_client_;
     rclcpp::Client<lifecycle_msgs::srv::GetState>::SharedPtr get_state_client_;
     rclcpp::Client<plansys2_msgs::srv::AddProblem>::SharedPtr add_problem_client_;
-    // Add callback for goal server
+    rclcpp::Subscription<jigless_planner_interfaces::msg::JointStatus>::SharedPtr joint_status_subscriber_;
+    rclcpp::Service<jigless_planner_interfaces::srv::InteractTop>::SharedPtr interact_top_service_;
+    rclcpp::TimerBase::SharedPtr step_timer_;
 
     void init();
-    bool init_knowledge();
+    bool initKnowledge();
+    std::unique_ptr<std::vector<plansys2_msgs::msg::ActionExecutionInfo>> get_executing_actions(
+      const std::vector<plansys2_msgs::msg::ActionExecutionInfo> &feedback);
+    void jointCallback(const jigless_planner_interfaces::msg::JointStatus & msg);
+    void topServiceCallback(
+      const std::shared_ptr<jigless_planner_interfaces::srv::InteractTop::Request> request,
+      const std::shared_ptr<jigless_planner_interfaces::srv::InteractTop::Response> response);
     void step(); // Runs the whole loop with state change based actions
     // bool response_callback(rclcpp::Client<plansys2_msgs::srv::AddProblem>::SharedFuture future);
     // void get_state_callback(rclcpp::Client<lifecycle_msgs::srv::GetState>::SharedFuture future);
     // void set_state_callback(rclcpp::Client<lifecycle_msgs::srv::ChangeState>::SharedFuture future);
-    
 
     unsigned int get_state();
     void set_state(unsigned int state);
--- src/TopControllerNode.cpp
+++ src/TopControllerNode.cpp
@@ -1,6 +1,5 @@
-#include "jigless-planner/TopControllerNode.hpp"
 #include <plansys2_pddl_parser/Utils.h>
-#include "TopControllerNode.hpp"
+#include "jigless-planner/TopControllerNode.hpp"
 
 namespace jigless_planner
 {
@@ -13,24 +12,52 @@ namespace jigless_planner
   TopControllerNode::~TopControllerNode()
   {
     RCLCPP_INFO(this->get_logger(), "Destroying top controller node");
+    if(step_timer_)
+      step_timer_->cancel();
+    RCLCPP_INFO(this->get_logger(), "Stopping plan execution");
+    if (executor_client_)
+      executor_client_->cancel_plan_execution();
+    if (problem_expert_) {
+      problem_expert_->clearGoal();
+      problem_expert_->clearKnowledge();
+    }
+    RCLCPP_INFO(this->get_logger(), "Shutting down bottom controller node");
+    if (change_state_client_)
+      set_state(lifecycle_msgs::msg::Transition::TRANSITION_DESTROY);
   }
 
   void TopControllerNode::init()
   {
     lifecycle_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     action_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    interactor_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    executor_group = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     domain_expert_ = std::make_shared<plansys2::DomainExpertClient>();
     planner_client_ = std::make_shared<plansys2::PlannerClient>();
     problem_expert_ = std::make_shared<plansys2::ProblemExpertClient>();
     executor_client_ = std::make_shared<plansys2::ExecutorClient>();
+    add_problem_client_ = this->create_client<plansys2_msgs::srv::AddProblem>(
+      "problem_expert/add_problem");
     change_state_client_ = this->create_client<lifecycle_msgs::srv::ChangeState>(
       "bottom_controller_node/change_state", rmw_qos_profile_services_default, lifecycle_group);
     get_state_client_ = this->create_client<lifecycle_msgs::srv::GetState>(
       "bottom_controller_node/get_state", rmw_qos_profile_services_default, lifecycle_group);
+    auto qos_setting = rclcpp::QoS(rclcpp::KeepLast(10));
+    qos_setting.reliable();
+    auto subscriber_opt = rclcpp::SubscriptionOptions();
+    subscriber_opt.callback_group = action_group;
+    joint_status_subscriber_ = this->create_subscription<jigless_planner_interfaces::msg::JointStatus>(
+      "failed_joints", qos_setting, std::bind(
+      &TopControllerNode::jointCallback, this, std::placeholders::_1), subscriber_opt);
+    interact_top_service_ = this->create_service<jigless_planner_interfaces::srv::InteractTop>("interact_top",
+      std::bind(&TopControllerNode::topServiceCallback, this, std::placeholders::_1,
+        std::placeholders::_2), rmw_qos_profile_services_default, interactor_group);
     this->declare_parameter("problem_file_path", "/home/mdh-es/multirobot_ws/src/jigless-planner/pddl/top_welding_problem.pddl");
+    step_timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(100), std::bind(&TopControllerNode::step, this), executor_group);
   }
 
-  bool TopControllerNode::init_knowledge()
+  bool TopControllerNode::initKnowledge()
   {
     using namespace std::literals::chrono_literals;
     std::string problem_file_path = this->get_parameter("problem_file_path").as_string();
@@ -52,8 +79,8 @@ namespace jigless_planner
       }
       RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
     }
-    auto result = add_problem_client_->async_send_request(request).future.share();
-    auto status = result.wait_for(3s);
+    auto future = add_problem_client_->async_send_request(request).future.share();
+    auto status = future.wait_for(3s);
     if (status == std::future_status::ready) {
       auto result = future.get();
       if (result->success){
@@ -66,6 +93,46 @@ namespace jigless_planner
     } 
     return false;
   };
+
+  void TopControllerNode::jointCallback(const jigless_planner_interfaces::msg::JointStatus &msg)
+  {
+    std::lock_guard<std::mutex> lock(failed_joints_mutex_);
+    RCLCPP_WARN(this->get_logger(), "Some joints failed to weld");
+    for (std::size_t i = 0; i < msg.joints.size(); ++i) {
+      if (!pause_)
+        pause_ = msg.status[i];
+      failed_joints[msg.joints[i]] = msg.status[i];
+    }
+  }
+
+  void TopControllerNode::topServiceCallback(
+    const std::shared_ptr<jigless_planner_interfaces::srv::InteractTop::Request> request,
+    const std::shared_ptr<jigless_planner_interfaces::srv::InteractTop::Response> response)
+  {
+    switch (request->operation)
+    {
+    case jigless_planner_interfaces::srv::InteractTop::Request::START: {
+      goal_joints = std::move(request->joints.joints);
+      break;
+    }
+    case jigless_planner_interfaces::srv::InteractTop::Request::ADD: {
+      goal_joints.reserve(goal_joints.size() + request->joints.joints.size());
+      for (std::size_t i = 0; i < request->joints.joints.size(); ++i) {
+        goal_joints.emplace_back(request->joints.joints[i]);
+      }
+      break;
+    }
+    case jigless_planner_interfaces::srv::InteractTop::Request::REMOVE: {
+      for (std::size_t i = 0; i < request->joints.joints.size(); ++i) {
+        goal_joints.erase(std::remove(goal_joints.begin(), goal_joints.end(), request->joints.joints[i]), goal_joints.end());
+      }
+      break;
+    }
+    default:
+      break;
+    }
+    response->success = true;
+  }
 
   unsigned int TopControllerNode::get_state()
   {
@@ -110,15 +177,27 @@ namespace jigless_planner
     }
   }
 
+  std::unique_ptr<std::vector<plansys2_msgs::msg::ActionExecutionInfo>> TopControllerNode::get_executing_actions(
+    const std::vector<plansys2_msgs::msg::ActionExecutionInfo> &feedback)
+  {
+    auto executing_actions = std::make_unique<std::vector<plansys2_msgs::msg::ActionExecutionInfo>>();
+    for (const auto &action : feedback) {
+      if (action.status == plansys2_msgs::msg::ActionExecutionInfo::EXECUTING) {
+        executing_actions->push_back(action);
+      }
+    }
+    return executing_actions;
+  }
+
   void TopControllerNode::step()
   {
     // Implement the state machine logic here
     switch (state_) {
-      case STARTING:{
-        if (init_knowledge()) {
+      case STARTING: {
+        if (initKnowledge()) {
           set_state(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
           state_ = READY;
-          RCLCPP_INFO(this->get_logger(), "Top controller node configured and ready")
+          RCLCPP_INFO(this->get_logger(), "Top controller node configured and ready");
         }
         break;
       }
@@ -133,12 +212,12 @@ namespace jigless_planner
           }
           goal_ss << "))";
           RCLCPP_INFO(this->get_logger(), "Received goal request");
-          problem_expert_->setGoal(goal_ss.str());
+          problem_expert_->setGoal(plansys2::Goal(goal_ss.str()));
           std::string domain = domain_expert_->getDomain();
           std::string problem = problem_expert_->getProblem();
-          auto plan = planner_client->getPlan(domain, problem);
+          auto plan = planner_client_->getPlan(domain, problem);
           if (!plan.has_value()) {
-            RCLCPP_ERROR_STREAM(this->get_logger(), "Could not find plan to reach goal " << goal_ss << std::endl); 
+            RCLCPP_ERROR_STREAM(this->get_logger(), "Could not find plan to reach goal " << goal_ss.str() << std::endl); 
             RCLCPP_WARN(this->get_logger(), "Clearing and waiting for new goal");
             problem_expert_->clearGoal();
             goal_joints.clear();
@@ -147,23 +226,102 @@ namespace jigless_planner
           if (executor_client_->start_plan_execution(plan.value())) {
             RCLCPP_INFO(this->get_logger(), "Plan started");
             state_ = RUNNING;
+            RCLCPP_INFO(this->get_logger(), "Switching to RUNNING state");
           } else {
             RCLCPP_ERROR(this->get_logger(), "Failed to start plan execution");
+            RCLCPP_WARN(this->get_logger(), "Clearing and waiting for new goal");
+            problem_expert_->clearGoal();
+            goal_joints.clear();
+            set_state(lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
           }
         }
         break;
       }
       case RUNNING: {
         // Perform actions for the RUNNING state
-        
+        if (executor_client_->execute_and_check_plan()) {
+          auto plan_result = executor_client_->getResult();
+          if (plan_result.value().success) {
+            RCLCPP_INFO(this->get_logger(), "Plan successfully finished");
+          } else {
+            RCLCPP_ERROR(this->get_logger(), "Plan finished with error");
+          }
+          RCLCPP_INFO(this->get_logger(), "Clearing goal");
+          problem_expert_->clearGoal();
+          goal_joints.clear();
+          RCLCPP_INFO(this->get_logger(), "Switching to READY state");
+          state_ = READY;
+          break;
+        }
+        {
+          std::lock_guard<std::mutex> lock(failed_joints_mutex_);
+          if (pause_) {
+            state_ = PAUSED;
+            break;
+          }
+        }
+        if (goal_changed_) {
+          RCLCPP_INFO(this->get_logger(), "Goal changed, stopping execution");
+          executor_client_->cancel_plan_execution();
+          state_ = STOPPED; 
+          break;
+        }
+        RCLCPP_INFO(this->get_logger(), "Running plan execution");
+        auto current_actions = get_executing_actions(executor_client_->getFeedBack().action_execution_status);
+        for (const auto &action : *current_actions) {
+          RCLCPP_INFO(this->get_logger(), "Executing action: %s with progress: %f", action.action_full_name.c_str(), action.completion);
+        }
+        break;
       }
-        break;
-      case PAUSED:
+      case PAUSED: {
         // Perform actions for the PAUSED state
+        RCLCPP_WARN(this->get_logger(), "Paused as some joints failed to weld");
+        RCLCPP_WARN(this->get_logger(), "Cancelling plan execution");
+        executor_client_->cancel_plan_execution();
+        std::unique_lock<std::mutex> joints_lock(failed_joints_mutex_);
+        std::map<std::string, bool> failed_joints_cp = std::move(failed_joints); // Check if failed_joints is empty after this
+        pause_ = false;
+        joints_lock.unlock();
+        for (const auto &pair : failed_joints_cp) {
+          if (pair.second) {
+            RCLCPP_WARN(this->get_logger(), "Failed joint: %s", pair.first.c_str());
+            RCLCPP_WARN(this->get_logger(), "Resetting joint status");
+            problem_expert_->removePredicate(plansys2::Predicate("(welded " + pair.first + ")"));
+            problem_expert_->addPredicate(plansys2::Predicate("(not_welded " + pair.first + ")"));
+            problem_expert_->removePredicate(plansys2::Predicate("(commanded " + pair.first + ")"));
+          } else {
+            RCLCPP_INFO(this->get_logger(), "Joint %s is ok", pair.first.c_str());
+            goal_joints.erase(std::remove(goal_joints.begin(), goal_joints.end(), pair.first), goal_joints.end());
+            problem_expert_->removePredicate(plansys2::Predicate("(not_welded " + pair.first + ")"));
+            problem_expert_->addPredicate(plansys2::Predicate("(welded " + pair.first + ")"));
+          }
+        }
+        RCLCPP_INFO(this->get_logger(), "Updated predicates");
+        RCLCPP_INFO(this->get_logger(), "Clearing goal for replanning under new circumstances");
+        problem_expert_->clearGoal();
+        state_ = READY;
+        RCLCPP_INFO(this->get_logger(), "Switching to READY state");
         break;
-      case STOPPED:
+      }
+      case STOPPED: {
         // Perform actions for the STOPPED state
+        RCLCPP_INFO(this->get_logger(), "Deactivating bottom controller");
+        set_state(lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
+        RCLCPP_INFO(this->get_logger(), "Clearing goal");
+        problem_expert_->clearGoal();
+        RCLCPP_INFO(this->get_logger(), "Switching to READY state");
+        state_ = READY;
         break;
+      }
     }
   }
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<jigless_planner::TopControllerNode>("top_controller_node");
+  rclcpp::spin(node->get_node_base_interface());
+  rclcpp::shutdown();
+  return 0;
 }
